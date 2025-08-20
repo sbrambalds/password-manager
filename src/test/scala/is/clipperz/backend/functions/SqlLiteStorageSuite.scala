ee4e3d79477f26112207ab6e33bef6d31d7c7deb
sbrambalds <riccardo.rambaldi4@studio.unibo.it>
2025-08-20 16:54:17 +0200

    add test for UserDb and OneTimeShareDb in SqlLiteStorageSuite

--- src/test/scala/is/clipperz/backend/functions/SqlLiteStorageSuite.scala
+++ src/test/scala/is/clipperz/backend/functions/SqlLiteStorageSuite.scala
@@ -2,28 +2,23 @@ package is.clipperz.backend.functions
 
 import java.security.MessageDigest
 import scala.language.postfixOps
-import zio.{ Chunk, ZIO }
-import zio.stream.{ ZStream, ZSink }
-import zio.test.Assertion.{ nothing, throws, throwsA, fails, isSubtype, anything }
-import zio.test.{ ZIOSpecDefault, assertTrue, assert, assertCompletes, assertZIO, TestAspect }
+import zio.{ Chunk, ZIO, Scope, Exit }
+import zio.stream.ZStream
+import zio.test.*
+import zio.test.Assertion.*
 import zio.json.EncoderOps
-import zio.http.{ Version, Headers, Method, URL, Request, Body }
 import zio.http.*
-import zio.nio.file.{ FileSystem }
+import zio.nio.file.FileSystem
 import is.clipperz.backend.Main
 import _root_.is.clipperz.backend.Exceptions.*
 import zio.Clock
-import zio.Clock.ClockLive
 import zio.test.TestClock
 import zio.Duration
 import is.clipperz.backend.TestUtilities
-import is.clipperz.backend.sqlite.* 
-import zio.test.TestResult.{ allSuccesses }
-import com.zaxxer.hikari.HikariConfig
-import com.zaxxer.hikari.HikariDataSource
+import is.clipperz.backend.sqlite.*
+import com.zaxxer.hikari.{HikariConfig, HikariDataSource}
 import com.augustnagro.magnum.magzio.Transactor
-import zio.Scope
-import zio.Exit
+import com.augustnagro.magnum.magzio.*
 
 object SqlLiteStorageSpec extends ZIOSpecDefault:
 
@@ -32,61 +27,83 @@ object SqlLiteStorageSpec extends ZIOSpecDefault:
   config.setDriverClassName("org.sqlite.JDBC")
   config.setMaximumPoolSize(5)
   config.setConnectionTestQuery("SELECT 1")
-  
-  val dataSource = new HikariDataSource(config)
 
-  val keyValueStorage = KeyValueStorage.SqlLiteKeyValueStorage[BlobDb](new BlobRepo(), BlobDb.apply)
-    .provideLayer(Transactor.layer(dataSource))
-  
-  
-  val testContent = ZStream.fromIterable("testContent".getBytes().nn)
-  val testMetadata =  ZStream.fromIterable("testMetadata".getBytes().nn)
+  val dataSource = new HikariDataSource(config)
+  val transactor = Transactor.layer(dataSource)
+
+  val testContent  = ZStream.fromIterable("testContent".getBytes.nn)
+  val testMetadata = ZStream.fromIterable("testMetadata".getBytes.nn)
   val failingContent = ZStream.never
   val testKey = "testKey"
   val failingKey = "failingKey"
 
-  def spec = suite("SqlLiteValueStorage")(
-    test("getBlob - fail") {
-      assertZIO(keyValueStorage.flatMap(_.getBlob(testKey).exit))(fails(isSubtype[ResourceNotFoundException](anything)))
-    } +
-    test("saveBlob - success") {
+  def storageSuite[T <: DbTable](name: String, repo: Repo[T, T, Key], ctor: (Key, String, Array[Byte]) => T) =
+    suite(s"SqlLiteValueStorage - $name")(
+      test("getBlob - fail") {
+        val keyValueStorage =
+          KeyValueStorage.SqlLiteKeyValueStorage[T](repo, ctor).provideLayer(transactor)
+
+        assertZIO(keyValueStorage.flatMap(_.getBlob(testKey).exit))(fails(isSubtype[ResourceNotFoundException](anything)))
+      },
+      test("saveBlob - success") {
+        val keyValueStorage =
+          KeyValueStorage.SqlLiteKeyValueStorage[T](repo, ctor).provideLayer(transactor)
+
         for {
           _ <- keyValueStorage.flatMap(_.saveBlobWithMetadata(testKey, testContent, testMetadata, false))
           _ <- TestClock.adjust(Duration.fromMillis(KeyValueStorage.WAIT_TIME + 10))
           (content, _) <- keyValueStorage.flatMap(_.getBlob(testKey))
           result <- testContent.zip(content).map((a, b) => a == b).toIterator.map(_.map(_.getOrElse(false)).reduce(_ && _))
         } yield assertTrue(result)
-    } +
-    test("saveBlob with failing stream - success") {
+      },
+      test("saveBlob with failing stream - success") {
+        val keyValueStorage =
+          KeyValueStorage.SqlLiteKeyValueStorage[T](repo, ctor).provideLayer(transactor)
+
         for {
           fiber <- keyValueStorage.flatMap(_.saveBlob(failingKey, failingContent, false).fork)
-          _ <- TestClock.adjust(Duration.fromMillis(KeyValueStorage.WAIT_TIME + 10))
-          res <- assertZIO(fiber.await)(fails(isSubtype[EmptyContentException](anything)))
+          _     <- TestClock.adjust(Duration.fromMillis(KeyValueStorage.WAIT_TIME + 10))
+          res   <- assertZIO(fiber.await)(fails(isSubtype[EmptyContentException](anything)))
         } yield res
-    } +
-    test("getBlob - success") {
+      },
+      test("getBlob - success") {
+        val keyValueStorage =
+          KeyValueStorage.SqlLiteKeyValueStorage[T](repo, ctor).provideLayer(transactor)
+
         for {
           (content, _) <- keyValueStorage.flatMap(_.getBlob(testKey))
           result <- testContent.zip(content).map((a, b) => a == b).toIterator.map(_.map(_.getOrElse(false)).reduce(_ && _))
         } yield assertTrue(result)
-    } +
-    test("deleteBlob - success") {
+      },
+      test("deleteBlob - success") {
+        val keyValueStorage =
+          KeyValueStorage.SqlLiteKeyValueStorage[T](repo, ctor).provideLayer(transactor)
+
         for {
-            _   <- keyValueStorage.flatMap(_.getBlob(testKey))
-            _   <- keyValueStorage.flatMap(_.deleteBlob(testKey))
-            res <- assertZIO(keyValueStorage.flatMap(_.getBlob(testKey).exit))(fails(isSubtype[ResourceNotFoundException](anything)))
-        } yield allSuccesses(assertCompletes, res) 
-    } +
-    test("deleteBlob - nothing to delete") {
+          _   <- keyValueStorage.flatMap(_.getBlob(testKey))
+          _   <- keyValueStorage.flatMap(_.deleteBlob(testKey))
+          res <- assertZIO(keyValueStorage.flatMap(_.getBlob(testKey).exit))(fails(isSubtype[ResourceNotFoundException](anything)))
+        } yield TestResult.allSuccesses(assertCompletes, res)
+      },
+      test("deleteBlob - nothing to delete") {
+        val keyValueStorage =
+          KeyValueStorage.SqlLiteKeyValueStorage[T](repo, ctor).provideLayer(transactor)
+
         for {
-            res <- assertZIO(keyValueStorage.flatMap(_.getBlob(testKey).exit))(fails(isSubtype[ResourceNotFoundException](anything)))
-            _   <- keyValueStorage.flatMap(_.deleteBlob(testKey))
-        } yield allSuccesses(res, assertCompletes)
-    }
-  ) @@ TestAspect.sequential @@
-    TestAspect.beforeAll({
-      for {
-        _ <- TestUtilities.dropTable()
-                          .provideLayer(Transactor.layer(dataSource))
-      } yield()
+          res <- assertZIO(keyValueStorage.flatMap(_.getBlob(testKey).exit))(fails(isSubtype[ResourceNotFoundException](anything)))
+          _   <- keyValueStorage.flatMap(_.deleteBlob(testKey))
+        } yield TestResult.allSuccesses(res, assertCompletes)
+      }
+    ) @@ TestAspect.sequential 
+    @@ TestAspect.beforeAll({
+      for { 
+        _ <- TestUtilities.dropTable() 
+              .provideLayer(Transactor.layer(dataSource)) 
+      } yield() 
     })
+
+  def spec = suite("All Storages")(
+    storageSuite("UserDb", new UserRepo(), UserDb.apply),
+    storageSuite("BlobDb", new BlobRepo(), BlobDb.apply),
+    storageSuite("OneTimeShareDb", new OneTimeShareRepo(), OneTimeShareDb.apply)
+  )
